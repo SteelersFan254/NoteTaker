2e28b0eabbd2964fbd72babe89354d05d3bb1e2a
Ryan Borja <rcborja.school@gmail.com>
2020-01-27 19:10:28 -0600

    finally finished the post

--- back.js
+++ back.js
@@ -8,12 +8,33 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+app.use(express.static("db"));
+
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    var notes = fs.readFileSync(path.join(__dirname, "./db/db.json"));
+    res.send(notes)
+    console.log(notes)
+});
+
+app.post("/api/notes", function (req, res) {
+    var saveNote = req.body;
+    fs.readFile(path.join(__dirname, "/db/db.json"), function(err, data) {
+        var parseData = JSON.parse(data);
+        parseData.push(saveNote);
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(parseData), "UTF8", (error) =>{
+            if (error) throw error;
+        });
+    });
+    return res.send(saveNote)
 });
 
 
